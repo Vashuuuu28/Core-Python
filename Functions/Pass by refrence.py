7bb92609fd16b4c434aa220cea7a7f67b4fa563e
Vashuuuu28 <aditya0128.bilgaiyan@gmail.com>
2026-03-31 15:12:26 +0530

    update

--- Functions/Pass by refrence.py
+++ Functions/Pass by refrence.py
@@ -1,3 +1,28 @@
-def changedlist(list):
-    list.append(15);
+def changelist(list):
+    list.append(6);
+    print(list)
+    return
+
+
+
+
+
+list  = [1,2,3,4,5]
+print(list )
+changelist(list)
 print(list)
+
+
+
+
+
+
+
+
+
+
+
+
+# def changedlist(list):
+#     list.append(15);
+# print(list)
