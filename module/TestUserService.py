afe3558f0fe0f01b30192cc0ca25bcb13a0e615c
Vashuuuu28 <aditya0128.bilgaiyan@gmail.com>
2026-04-01 15:24:31 +0530

    update

---
+++ module/TestUserService.py
@@ -0,0 +1,4 @@
+import UserService
+
+UserService.add("aditya","bilgaiyan")
+UserService.update("vashu","bilgaiyan")
---
+++ module/UserService.py
@@ -0,0 +1,10 @@
+def add(firstname,lastname):
+    print("adding user")
+    print("name",firstname,lastname)
+    return
+
+def update(firstname,lastname):
+    print("updating user")
+    print("name",firstname,lastname)
+    return
+
